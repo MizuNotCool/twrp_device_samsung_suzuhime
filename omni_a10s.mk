b2f348934ca7e3e6e228f0d92dc3697c62b9c0b6
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-11-12 13:08:13 +0800

    suzuhime : change codename experiment

--- omni_a10s.mk
+++ omni_a10s.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from a10s device
-$(call inherit-product, device/samsung/a10s/device.mk)
+$(call inherit-product, device/samsung/suzuhime/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a10s
-PRODUCT_NAME := omni_a10s
+PRODUCT_DEVICE := suzuhime
+PRODUCT_NAME := omni_suzuhime
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy A10s
 
